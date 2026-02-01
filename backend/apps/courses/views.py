c7d7b466487dada431826c2ea8f0e06185926dc9
SeM0nchik <semone.pavlychev@gmail.com>
2026-02-01 21:52:15 +0300

    update: updated course list response format

--- backend/apps/courses/views.py
+++ backend/apps/courses/views.py
@@ -11,4 +11,13 @@ class CourseList(generics.ListAPIView):
     serializer_class = CourseSerializer
     def get_queryset(self):
         queryset = Course.objects.all()
-        return queryset[:6]
+        return queryset
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+
+        return Response({
+            'number_of_courses': len(serializer.data),
+            'data': serializer.data
+        })

7b0ab856ea8b27ab3e538aa53f10a4260b94e6dd
SeM0nchik <semone.pavlychev@gmail.com>
2026-02-01 21:33:35 +0300

    update: updated course list view

--- backend/apps/courses/views.py
+++ backend/apps/courses/views.py
@@ -1,23 +1,14 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+
 from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework.viewsets import ModelViewSet
+
 
 from .models import Course
 from .serializers import CourseSerializer
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status, generics
 
 
-class CourseList(APIView):
-    def get(self, request, format=None):
-        courses = Course.objects.all()
-        serializer = CourseSerializer(courses, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format = None):
-        serializer = CourseSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class CourseList(generics.ListAPIView):
+    serializer_class = CourseSerializer
+    def get_queryset(self):
+        queryset = Course.objects.all()
+        return queryset[:6]

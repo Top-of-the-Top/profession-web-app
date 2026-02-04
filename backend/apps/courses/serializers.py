429acaa3796e9131696608b34d7682d334daf0ee
SeM0nchik <semone.pavlychev@gmail.com>
2026-02-04 17:19:42 +0300

    update: updated image url in course serializer

--- backend/apps/courses/serializers.py
+++ backend/apps/courses/serializers.py
@@ -1,22 +1,10 @@
 from .models import Course
 from rest_framework import serializers
-from django.conf import settings
 
 
 class CourseSerializer(serializers.ModelSerializer):
-    image_url = serializers.SerializerMethodField()
+    image_url = serializers.ReadOnlyField()
 
     class Meta:
         model = Course
-        fields = ['course_id', 'title', 'price', 'image', 'image_url']
-
-    def get_image_url(self, obj):
-        if obj.image:
-            if settings.USE_S3:
-                return obj.image.url
-            else:
-                request = self.context.get('request')
-                if request:
-                    return request.build_absolute_uri(obj.image.url)
-                return obj.image.url
-        return None
+        fields = ['course_id', 'title', 'price', 'image_url']

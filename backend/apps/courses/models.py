9b34129690dc67f06f31717a92119b6ee3edddf6
SeM0nchik <semone.pavlychev@gmail.com>
2026-02-04 17:16:31 +0300

    add: add correct save and delete signals

--- backend/apps/courses/models.py
+++ backend/apps/courses/models.py
@@ -1,11 +1,12 @@
 from django.db import models
-from uuid import uuid4
-
+from django.db.models.signals import pre_delete, pre_save
+import os
 from django.dispatch import receiver
 
 DEFAULT_COURSE_IMAGE = "courses/default_course.png"
+
+
 def course_image_path(instance, filename):
-    """Сохраняет изображение как course_<id>.jpg"""
     ext = filename.split('.')[-1].lower()
     return f'courses/course_{instance.pk}.{ext}'
 
@@ -23,17 +24,53 @@ class Course(models.Model):
         default=DEFAULT_COURSE_IMAGE,
     )
 
+    @property
+    def image_url(self):
+        if self.image and self.image.name != DEFAULT_COURSE_IMAGE:
+            return self.image.url
+        bucket = os.getenv("AWS_S3_BUCKET_NAME", "your-bucket")
+        return f'https://storage.yandexcloud.net/{bucket}/{DEFAULT_COURSE_IMAGE}'
+
     def save(self, *args, **kwargs):
-        super(Course, self).save(*args, **kwargs)
-
-        if self.image:
-            new_name = course_image_path(self,self.image.name)
-            self.image.name = new_name
+        is_new = self.pk is None
+        
+        if is_new and self.image and hasattr(self.image, 'file') and self.image.name != DEFAULT_COURSE_IMAGE:
+            image_file = self.image.file
+            original_name = getattr(self.image, 'name', 'image.jpg')
+            
+            self.image = None
+            super().save(*args, **kwargs)
+            
+            ext = original_name.split('.')[-1].lower() if '.' in original_name else 'jpg'
+            new_name = f'courses/course_{self.pk}.{ext}'
+            
+            image_file.seek(0)
+            self.image.save(new_name, image_file, save=False)
             super().save(update_fields=['image'])
-
+        else:
+            super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
+@receiver(pre_save, sender=Course)
+def handle_course_image_update(sender, instance, **kwargs):
+    if not instance.pk:
+        return
+
+    try:
+        old_instance = sender.objects.get(pk=instance.pk)
+        if (old_instance.image and old_instance.image.name != DEFAULT_COURSE_IMAGE and
+                instance.image and instance.image != old_instance.image):
+            old_instance.image.delete(save=False)
+    except sender.DoesNotExist:
+        pass
+
+
+@receiver(pre_delete, sender=Course)
+def delete_course_image(sender, instance, **kwargs):
+    if instance.image and instance.image.name != DEFAULT_COURSE_IMAGE:
+        instance.image.delete(save=False)
+
 
 
 class Lesson(models.Model):

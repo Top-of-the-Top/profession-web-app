637e3de32071cb3f496c8a44d3c1e00dbfaa2a54
SeM0nchik <semone.pavlychev@gmail.com>
2026-02-12 01:15:01 +0300

    refactor: add new api directory for courses

--- backend/apps/courses/api/views.py
+++ backend/apps/courses/api/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 
 from .models import Course
 from .serializers import CourseDTOSerializer
-from rest_framework import viewsets, status, generics
+from rest_framework import generics
 
 
 class CourseDTOList(generics.ListAPIView):

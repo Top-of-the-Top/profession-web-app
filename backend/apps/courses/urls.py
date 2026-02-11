4f9ca6d0fd0ae4abe441e16ec3461e4b6cfef115
SeM0nchik <semone.pavlychev@gmail.com>
2026-02-12 00:56:41 +0300

    feature: implement new routes using dto list view

--- backend/apps/courses/urls.py
+++ backend/apps/courses/urls.py
@@ -1,7 +1,8 @@
 # courses/urls.py
 from django.urls import path
-from .views import CourseList
+from .views import CourseDTOList
 
 urlpatterns = [
-    path('landing/courses/', CourseList.as_view(), name='course-list'),
+    path('landing/courses/', CourseDTOList.as_view(), name='course-list-preview'),
+    path('app/store/', CourseDTOList.as_view(), name='course-list-store'),
 ]

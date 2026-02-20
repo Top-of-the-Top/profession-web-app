d4bd06f3cdd0521322671614158570c8417bb2cc
SeM0nchik <semone.pavlychev@gmail.com>
2026-02-20 23:13:11 +0300

    chore: update postgres settings

--- backend/apps/cart/migrations/0001_initial.py
+++ backend/apps/cart/migrations/0001_initial.py
@@ -28,7 +28,7 @@ class Migration(migrations.Migration):
             name='CartItem',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('cart_id', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='cart.cart')),
+                ('cart_id', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='carts.carts')),
                 ('course_id', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='courses.course')),
             ],
             options={
@@ -37,8 +37,8 @@ class Migration(migrations.Migration):
             },
         ),
         migrations.AddField(
-            model_name='cart',
+            model_name='carts',
             name='courses',
-            field=models.ManyToManyField(through='cart.CartItem', to='courses.course'),
+            field=models.ManyToManyField(through='carts.CartItem', to='courses.course'),
         ),
     ]
--- backend/project/settings.py
+++ backend/project/settings.py
@@ -103,14 +103,16 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-import dj_database_url
 import os
 DATABASES = {
-    "default": dj_database_url.parse(
-        os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=False,
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+    }
 }
 
 

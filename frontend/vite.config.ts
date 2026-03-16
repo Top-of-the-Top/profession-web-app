f98bcf3654b164cc35187b1a1d9bc1f9e6eb0f6a
Артём Щербаков <artemsch1711@gmail.com>
2026-03-16 19:37:17 +0300

    hotfix(frontend): fix vite hmr conflict

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -17,13 +17,13 @@ export default defineConfig({
 	server: {
 		allowedHosts: ['professionkid.ru', 'www.professionkid.ru'],
 		host: '0.0.0.0',
-		port: 3000,
-		watch: {
-			usePolling: true,
-		},
-		hmr: {
-			clientPort: 3000,
-		},
+		// port: 3000,
+		// watch: {
+		// 	usePolling: true,
+		// },
+		// hmr: {
+		// 	clientPort: 3000,
+		// },
 	},
 	
 });

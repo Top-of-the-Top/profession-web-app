be3e990f0466ebcdf3286c54423e93ef3a0e0356
KomokPol <polina18.komkova@gmail.com>
2026-04-12 20:33:40 +0300

    fix: _ to -

--- backend/apps/users/api/urls.py
+++ backend/apps/users/api/urls.py
@@ -1,17 +1,21 @@
 from django.urls import path
-from .views import *
+from .views import (
+  RegisterView, RegisterView, LoginView, RefreshTokenView,
+  ResetPasswordView, RecoverPasswordPhoneView, RecoverPasswordView, 
+  ProfileView, VerifyEmailChangeView, VerifyPhoneChangeView
+)
 
 app_name = 'users'
 
 urlpatterns = [
     path('auth/register/', RegisterView.as_view(), name='register'),
-    path('auth/register/verify/', VerifyRegisterView.as_view(), name='register_verify'),
+    path('auth/register/verify/', RegisterView.as_view(), name='register-verify'),
     path('auth/login/', LoginView.as_view(), name='login'),
-    path('auth/token/refresh/', RefreshTokenView.as_view(), name='token_refresh'),
+    path('auth/token/refresh/', RefreshTokenView.as_view(), name='token-refresh'),
     path('auth/reset/', ResetPasswordView.as_view(), name='reset'),
     path('auth/recover/set/', RecoverPasswordView.as_view(), name='recover_set'),
-    path('auth/recover/phone/', RecoverPasswordPhoneView.as_view(), name='recover_phone'),
+    path('auth/recover/phone/', RecoverPasswordPhoneView.as_view(), name='recover-phone'),
     path('app/profile/', ProfileView.as_view(), name='profile'),
-    path('app/profile/verify_email/', VerifyEmailChangeView.as_view(), name='verify_email'),
-    path('app/profile/verify_phone/', VerifyPhoneChangeView.as_view(), name='verify_phone'),
+    path('app/profile/verify-email/', VerifyEmailChangeView.as_view(), name='verify-email'),
+    path('app/profile/verify-phone/', VerifyPhoneChangeView.as_view(), name='verify-phone'),
 ]

6362b2f5c8439b0897b3b52795f72db994c1f256
SeM0nchik <semone.pavlychev@gmail.com>
2026-04-15 01:09:26 +0300

    update: register models in admin panel

--- backend/apps/ai_chat_bot/admin.py
+++ backend/apps/ai_chat_bot/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Chunk, ChatSession, ChatMessage
 
-# Register your models here.
+@admin.register(Chunk)
+class ChunkAdmin(admin.ModelAdmin):
+  list_display = ['course', 'lesson', 'content']
+  list_filter = ['course', 'lesson']
+  search_fields = ['course__title', 'lesson__title', 'content']
+
+@admin.register(ChatSession)
+class ChatSessionAdmin(admin.ModelAdmin):
+  list_display = ['user', 'course']
+  list_filter = ['user', 'course']
+  search_fields = ['user__first_name', 'user__last_name', 'course__title']
+
+@admin.register(ChatMessage)
+class ChatMessageAdmin(admin.ModelAdmin):
+  list_display = ['chat_session', 'content', 'role']
+  list_filter = ['chat_session', 'role']
+  search_fields = ['chat_session__user__first_name', 'chat_session__user__last_name', 'chat_session__course__title', 'content']

6768782ec1cbb1ca4c06d45835f6b185cc88f98f
KomokPol <polina18.komkova@gmail.com>
2026-04-23 12:40:41 +0300

    add: replace webinar, webinar token and recording list item serializers

--- backend/apps/courses/api/serializers.py
+++ backend/apps/courses/api/serializers.py
@@ -9,8 +9,6 @@ from ..models import (
     Section,
     Question,
     Task,
-    Webinar,
-    Recording,
     PublishableMixin,
 )
 from ..lesson_content import resolve_lesson_document_string, parse_content_value
@@ -163,6 +161,8 @@ class LessonDetailReadSerializer(serializers.ModelSerializer):
 
     @extend_schema_field(LessonContentReadSerializer)
     def get_content(self, obj):
+        from apps.webinars.api.serializers import RecordingListItemSerializer
+        
         include_drafts = self.context.get('include_drafts', False)
         hws = filter_homework_queryset_for_visibility(
             obj.homework_set.all(), include_drafts
@@ -528,18 +528,6 @@ class QuestionSerializer(serializers.ModelSerializer):
         )
 
 
-class WebinarSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Webinar
-        fields = [
-            'webinar_id', 'lesson', 'status',
-            'started_by', 'started_at', 'ended_at',
-        ]
-        read_only_fields = [
-            'webinar_id', 'started_by', 'started_at', 'ended_at',
-        ]
-
-
 class UserWebinarListItemSerializer(serializers.Serializer):
     course_title = serializers.CharField()
     course_slug = serializers.CharField()
@@ -547,41 +535,3 @@ class UserWebinarListItemSerializer(serializers.Serializer):
     lesson_slug = serializers.CharField()
     started_at = serializers.DateTimeField(allow_null=True)
     ended_at = serializers.DateTimeField(allow_null=True)
-
-
-class WebinarTokenSerializer(serializers.Serializer):
-    rtc_token = serializers.CharField()
-    agora_app_id = serializers.CharField()
-    channel_name = serializers.CharField()
-    uid = serializers.IntegerField()
-    whiteboard_app_id = serializers.CharField()
-    whiteboard_room_uuid = serializers.CharField()
-    whiteboard_room_token = serializers.CharField()
-    whiteboard_region = serializers.CharField()
-    role = serializers.CharField()
-
-
-class RecordingListItemSerializer(serializers.ModelSerializer):
-    kinescope_embed_url = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Recording
-        fields = (
-            'recording_id',
-            'started_at',
-            'ended_at',
-            'status',
-            'kinescope_upload_status',
-            'kinescope_embed_url',
-            'whiteboard_pdf_url',
-        )
-
-    def get_kinescope_embed_url(self, obj):
-        if obj.kinescope_upload_status != 'ready' or not obj.kinescope_video_id:
-            return ''
-        request = self.context.get('request')
-        if not request or not request.user.is_authenticated:
-            return ''
-        from .utils.kinescope_utils import generate_drm_token
-        drm_token = generate_drm_token(user_id=request.user.pk, video_id=obj.kinescope_video_id)
-        return f'https://kinescope.io/embed/{obj.kinescope_video_id}?drmauthtoken={drm_token}'

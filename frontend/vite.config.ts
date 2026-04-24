d7c6d4015c3a2c3a1919ff02d794000efd436703
SeM0nchik <semone.pavlychev@gmail.com>
2026-04-25 00:14:52 +0300

    add: add new sites for testing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,28 +7,30 @@ export default defineConfig({
   resolve: {
     alias: {
       "@app": path.resolve(__dirname, "src/app"),
-			"@assets": path.resolve(__dirname, "./src/assets"),
-			"@entities": path.resolve(__dirname, "./src/entities"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@entities": path.resolve(__dirname, "./src/entities"),
       "@pages": path.resolve(__dirname, "./src/pages"),
       "@router": path.resolve(__dirname, "./src/router"),
       "@schemas": path.resolve(__dirname, "./src/schemas"),
-			"@shared": path.resolve(__dirname, "src/shared"),
+      "@shared": path.resolve(__dirname, "src/shared"),
       "@widgets": path.resolve(__dirname, "./src/widgets"),
       "@components": path.resolve(__dirname, "./src/components"),
     },
   },
-	server: {
-		allowedHosts: [
-        'professionkid.ru',
-        'www.professionkid.ru',
-        'unequal-wildfowl-dreamy.ngrok-free.dev',
-        ],
-        host: '0.0.0.0',
-        proxy: {
-            '/api': {
-                target: 'http://backend:9000',
-                changeOrigin: true,
-            },
-        },
-	},
+  server: {
+    allowedHosts: [
+      'professionkid.ru',
+      'www.professionkid.ru',
+      'unequal-wildfowl-dreamy.ngrok-free.dev',
+      'professionkid-testing.ru',
+      'www.professionkid-testing.ru',
+    ],
+    host: '0.0.0.0',
+    proxy: {
+      '/api': {
+        target: 'http://backend:9000',
+        changeOrigin: true,
+      },
+    },
+  },
 });

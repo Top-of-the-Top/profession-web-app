61d0f7a14256f59bc16e754bdcfd38a71cc0d93f
SeM0nchik <semone.pavlychev@gmail.com>
2026-04-26 16:45:39 +0300

    add: add new methods for chat service

--- backend/apps/ai_chat_bot/services/chat_services.py
+++ backend/apps/ai_chat_bot/services/chat_services.py
@@ -7,34 +7,37 @@ from apps.ai_chat_bot.services.base_service import YandexAIBase
 
 logger = logging.getLogger(__name__)
 class YandexChatAIService(YandexAIBase):
+    session: Session
     
     async def get_or_create_session(self, user, course):
-      logger.info(
-          "Establishing connection to new chat session in course %s for user %s",
-          course.pk,
-          user.pk,
-      )
+        logger.info(
+            "Establishing connection to new chat session in course %s for user %s",
+            course.pk,
+            user.pk,
+        )
 
-      session, created = await sync_to_async(
-          Session.objects.get_or_create)(
-              course=course,
-              user=user
-          )
+        session, created = await sync_to_async(
+            Session.objects.get_or_create)(
+                course=course,
+                user=user
+            )
+        self.session = session
+        
+        if created:
+            logger.info("Created new chat session %s", session.session_id)
 
-      if created:
-          logger.info("Created new chat session %s", session.chat_session_id)
-
-      return session
+        return session
     
-    async def save_message(self, session, role, content):
+    async def save_message(self, chat,  role, content):
+        logger.info("Saving message %s", content[:50])
         return await sync_to_async(Message.objects.create)(
-            chat_session=session,
+            chat=chat,
             role=role,
             content=content
         )
     
-    async def ask_question_stream(self, session, text):
-        logger.info("Create response stream for session %s", session.chat_session_id)
+    async def ask_question_stream(self, text):
+        logger.info("Create response stream for session %s", self.session.session_id)
         try:
             async with self._semaphore:
                 response_stream = await self.client.responses.create(
@@ -51,15 +54,37 @@ class YandexChatAIService(YandexAIBase):
             logger.error(f"Error while creating a new response stream: {e}")
             raise 
 
-    async def get_chats(self, session):
+    async def get_chats(self):
+        logger.info("Getting all chats for session %s", self.session)
         get_chats_for_session = lambda x: list(x.chats.all().order_by('-updated_at'))
-        
-        return await sync_to_async(get_chats_for_session)(session)
+        chats = await sync_to_async(get_chats_for_session)(self.session)
+        return chats
 
-    async def get_chat_history(self, session, chat_id):
-        chat = await sync_to_async(Chat.objects.get(chat_id=chat_id))
+    async def _get_chat_for_current_session(self, chat_id):
+        return await sync_to_async(Chat.objects.get)(
+            chat_id=chat_id,
+            session=self.session,
+        )
+
+    async def get_chat_history(self, chat_id):
+        chat = await self._get_chat_for_current_session(chat_id)
+        logger.info("Getting chat history for chat %s", chat)
         get_chat_messages = lambda x: list(x.messages.all().order_by('-updated_at'))
         messages = await sync_to_async(get_chat_messages)(chat)
-
         return messages
+    
+    async def delete_chat(self, chat_id):
+        chat = await self._get_chat_for_current_session(chat_id)
+        logger.info("Deleting chat %s", chat)
+        del_chat = lambda x: Chat.objects.filter(chat_id=x.chat_id).delete()
+        await sync_to_async(del_chat)(chat)
+    
+    async def create_new_chat(self):
+        logger.info("Creating new chat for session %s", self.session)
+        create_fun = lambda x: Chat.objects.create(session=self.session)
+        chat = await sync_to_async(create_fun)(self.session)
+        logger.info("Successfully created new chat %s", chat)
+        
+        return chat 
+        
         
